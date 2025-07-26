ebcfcfb179520780e07cc9247d7cfe416093f841
patrickwitmer <patrick@witmerdesigns.com>
2025-07-26 16:06:07 -0600

    Chapter one begins, hello word, name, age excercise

---
+++ Chapter 1/yourFirstProgram.py
@@ -0,0 +1,20 @@
+print('Hello World')
+
+print('What is your name?') # Ask for their name.
+my_name = input('>')
+
+print('Good to meet you, ' + my_name)
+print('The length of your name is:')
+print(len(my_name))
+
+print('What is your age?') # Ask for their Age.
+my_age = input('>')
+int_age = int(my_age)
+
+if int_age >= 40:
+  print('You are over the hill guy.')
+else:
+  print('You still have some time.')  
+
+print('The length of your age is:')
+print(len(my_age))
