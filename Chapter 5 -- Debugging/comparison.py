ec8007af6f34e2af5db1036524e23599d854aa12
patrickwitmer <patrick@witmerdesigns.com>
2025-07-29 16:15:09 -0600

    adding lessons from chapter 5 and 6

---
+++ Chapter 5 -- Debugging/comparison.py
@@ -0,0 +1,18 @@
+supplies = ['pens', 'staplers', 'flamethrowers', 'binders']
+for index, item in enumerate(supplies):
+  print('Index ' + str(index) + ' in supplies is: ' + item)
+
+# Index 0 in supplies is: pens
+# Index 1 in supplies is: staplers
+# Index 2 in supplies is: flamethrowers
+# Index 3 in supplies is: binders
+
+
+supplies = ['pens', 'staplers', 'flamethrowers', 'binders']
+for i in range(len(supplies)):
+  print('Index ' + str(i) + ' in supplies is: ' + supplies[i])
+
+# Index 0 in supplies is: pens
+# Index 1 in supplies is: staplers
+# Index 2 in supplies is: flamethrowers
+# Index 3 in supplies is: binders
